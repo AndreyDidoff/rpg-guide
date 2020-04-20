11157890aa4e048de96182c453536ec0c572a6eb
Andrey Didoff <andrey.didoff@avdata.com.br>
2020-04-20 10:59:18 -0300

    Include CRUD guide

--- backend/src/Routes/routes_guide.js
+++ backend/src/Routes/routes_guide.js
@@ -13,24 +13,102 @@ routes_guide.post(
   "/guide",
   celebrate({
     [Segments.BODY]: Joi.object().keys({
-      nickname: Joi.string().required(),
-      passwd: Joi.string().required().min(8),
+      id_alignment: Joi.number().required(),
+      id_class: Joi.number().required(),
+      id_color_eye: Joi.number().required(),
+      id_color_hair: Joi.number().required(),
+      id_color_skin: Joi.number().required(),
+      id_race: Joi.number().required(),
+      id_size: Joi.number().required(),
+      id_user: Joi.number().required(),
+      age: Joi.number().required(),
+      charisma: Joi.number(),
+      constitution: Joi.number(),
+      dexterity: Joi.number(),
+      force: Joi.number(),
+      heigth: Joi.number().required(),
+      intelligence: Joi.number(),
+      money_copper: Joi.number(),
+      money_gold: Joi.number(),
+      money_platinum: Joi.number(),
+      money_silver: Joi.number(),
+      movement: Joi.number(),
+      name: Joi.string().required(),
+      secret_story: Joi.string(),
+      story: Joi.string(),
+      weight: Joi.number().required(),
+      wisdom: Joi.number(),
+      xp: Joi.number(),
     }),
   }),
   guide.create
 );
-// Get guide
+// Select All Guides
 routes_guide.get(
   "/guide",
   celebrate({
     [Segments.HEADERS]: Joi.object({
       authorization: Joi.string().required(),
     }).unknown(),
+    [Segments.BODY]: Joi.object().keys({
+      id_user: Joi.number().required(),
+    }),
   }),
-  guide.create
+  guide.select_all_guides
 );
-// Delete guide
-
+// Update Guide
+routes_guide.put(
+  "/guide/:id_guide",
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required(),
+    }).unknown(),
+    [Segments.PARAMS]: Joi.object().keys({
+      id_guide: Joi.number().required(),
+    }),
+    [Segments.BODY]: Joi.object().keys({
+      id_alignment: Joi.number().required(),
+      id_class: Joi.number().required(),
+      id_color_eye: Joi.number().required(),
+      id_color_hair: Joi.number().required(),
+      id_color_skin: Joi.number().required(),
+      id_race: Joi.number().required(),
+      id_size: Joi.number().required(),
+      age: Joi.number().required(),
+      charisma: Joi.number(),
+      constitution: Joi.number(),
+      dexterity: Joi.number(),
+      force: Joi.number(),
+      heigth: Joi.number().required(),
+      intelligence: Joi.number(),
+      money_copper: Joi.number(),
+      money_gold: Joi.number(),
+      money_platinum: Joi.number(),
+      money_silver: Joi.number(),
+      movement: Joi.number(),
+      name: Joi.string().required(),
+      secret_story: Joi.string(),
+      story: Joi.string(),
+      weight: Joi.number().required(),
+      wisdom: Joi.number(),
+      xp: Joi.number(),
+    }),
+  }),
+  guide.update_guide
+);
+// Delete Guide
+routes_guide.delete(
+  "/guide/:id_guide",
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required(),
+    }).unknown(),
+    [Segments.PARAMS]: Joi.object().keys({
+      id_guide: Joi.number().required(),
+    }),
+  }),
+  guide.delete_guide
+);
 // Mod guide
 
 // Export routes_guide
--- backend/src/controllers/guide.js
+++ backend/src/controllers/guide.js
@@ -5,6 +5,10 @@ const connection = require("../database/connect");
 const table_db = "RPG_guide";
 // Exports
 module.exports = {
+  /*
+   * Guide
+   */
+  // Create Guide
   async create(request, res) {
     // Pega itens do body
     const {
@@ -48,7 +52,7 @@ module.exports = {
         .where("name", name)
         .count();
       // Verificar se achou:
-      if (count["count(*)"] > 0) {
+      if (count_guide["count(*)"] === 0) {
         // criar insert do banco
         let insert = {
           id_alignment: id_alignment,
@@ -82,7 +86,34 @@ module.exports = {
         const [id] = await connection(table_db).insert(insert);
         // Resposta
         return res.json({
-          query: body,
+          query: {
+            id_alignment: id_alignment,
+            id_class: id_class,
+            id_color_eye: id_color_eye,
+            id_color_hair: id_color_hair,
+            id_color_skin: id_color_skin,
+            id_race: id_race,
+            id_size: id_size,
+            id_user: id_user,
+            age: age,
+            charisma: charisma,
+            constitution: constitution,
+            dexterity: dexterity,
+            force: force,
+            heigth: heigth,
+            intelligence: intelligence,
+            money_copper: money_copper,
+            money_gold: money_gold,
+            money_platinum: money_platinum,
+            money_silver: money_silver,
+            movement: movement,
+            name: name,
+            secret_story: secret_story,
+            story: story,
+            weight: weight,
+            wisdom: wisdom,
+            xp: xp,
+          },
           data: { id: id },
           msg: "SUCCESS",
         });
@@ -90,6 +121,236 @@ module.exports = {
         // Resposta
         return res.status(406).json({
           msg: "Nome já cadastrado",
+        });
+      }
+    } else {
+      // Resposta
+      return res.status(400).json({
+        msg: "Usuário não autorizado",
+      });
+    }
+  },
+  // Select All Guides
+  async select_all_guides(request, res) {
+    // Pega itens do body
+    const { id_user } = request.body;
+    // Pega parametros do Headers para variavel
+    const cod_user = request.headers.authorization;
+    // Consulta quantos tem no banco
+    const user = await connection("RPG_users")
+      .select("cod")
+      .where("id", id_user)
+      .limit(1);
+    // Veriricar se encotrou no banco o usuário
+    if (user.length > 0) {
+      if (user[0].cod === cod_user) {
+        // Consulta quantos tem no banco
+        const guides = await connection(table_db)
+          .select(
+            "id_alignment",
+            "id_class",
+            "id_color_eye",
+            "id_color_hair",
+            "id_color_skin",
+            "id_race",
+            "id_size",
+            "id_user",
+            "age",
+            "charisma",
+            "constitution",
+            "dexterity",
+            "force",
+            "heigth",
+            "intelligence",
+            "money_copper",
+            "money_gold",
+            "money_platinum",
+            "money_silver",
+            "movement",
+            "name",
+            "secret_story",
+            "story",
+            "weight",
+            "wisdom",
+            "xp"
+          )
+          .orderBy("name");
+        // Resposta
+        res.header("X-Total-Count", guides.length);
+        return res.json({
+          rows: guides.length,
+          data: guides,
+          msg: "SUCCESS",
+        });
+      } else {
+        // Resposta
+        return res.status(400).json({
+          msg: "Usuário não autorizado",
+        });
+      }
+    } else {
+      // Resposta
+      return res.status(400).json({
+        msg: "Usuário não autorizado",
+      });
+    }
+  },
+  // Update Guide
+  async update_guide(request, res) {
+    // Pega todos os paramentros da rota e colocar na variavel
+    const { id_guide } = request.params;
+    // Pega itens do body
+    const {
+      id_alignment,
+      id_class,
+      id_color_eye,
+      id_color_hair,
+      id_color_skin,
+      id_race,
+      id_size,
+      age,
+      charisma = 0,
+      constitution = 0,
+      dexterity = 0,
+      force = 0,
+      heigth,
+      intelligence = 0,
+      money_copper = 0,
+      money_gold = 0,
+      money_platinum = 0,
+      money_silver = 0,
+      movement = 0,
+      name,
+      secret_story = "",
+      story = "",
+      weight,
+      wisdom = 0,
+      xp = 0,
+    } = request.body;
+    // Pega parametros do Headers para variavel
+    const cod_user = request.headers.authorization;
+    // Consulta quantos tem no banco
+    const [count] = await connection("RPG_users")
+      .where("cod", cod_user)
+      .count();
+    // Veriricar se encotrou no banco o usuário
+    if (count["count(*)"] > 0) {
+      // Consultar se o usuário já tem um personagem com o mesmo nome:
+      const [count_guide] = await connection(table_db)
+        .where("id", id_guide)
+        .count();
+      // Verificar se achou:
+      if (count_guide["count(*)"] !== 0) {
+        await connection(table_db)
+          .update({
+            id_alignment: id_alignment,
+            id_class: id_class,
+            id_color_eye: id_color_eye,
+            id_color_hair: id_color_hair,
+            id_color_skin: id_color_skin,
+            id_race: id_race,
+            id_size: id_size,
+            age: age,
+            charisma: charisma,
+            constitution: constitution,
+            dexterity: dexterity,
+            force: force,
+            heigth: heigth,
+            intelligence: intelligence,
+            money_copper: money_copper,
+            money_gold: money_gold,
+            money_platinum: money_platinum,
+            money_silver: money_silver,
+            movement: movement,
+            name: name,
+            secret_story: secret_story,
+            story: story,
+            weight: weight,
+            wisdom: wisdom,
+            xp: xp,
+          })
+          .where({ id: id_guide });
+        // Resposta
+        return res.json({
+          query: {
+            id_alignment: id_alignment,
+            id_class: id_class,
+            id_color_eye: id_color_eye,
+            id_color_hair: id_color_hair,
+            id_color_skin: id_color_skin,
+            id_race: id_race,
+            id_size: id_size,
+            age: age,
+            charisma: charisma,
+            constitution: constitution,
+            dexterity: dexterity,
+            force: force,
+            heigth: heigth,
+            intelligence: intelligence,
+            money_copper: money_copper,
+            money_gold: money_gold,
+            money_platinum: money_platinum,
+            money_silver: money_silver,
+            movement: movement,
+            name: name,
+            secret_story: secret_story,
+            story: story,
+            weight: weight,
+            wisdom: wisdom,
+            xp: xp,
+          },
+          msg: "SUCCESS",
+        });
+      } else {
+        // Resposta
+        return res.status(406).json({
+          msg: "Personagem não encontrado",
+        });
+      }
+    } else {
+      // Resposta
+      return res.status(400).json({
+        msg: "Usuário não autorizado",
+      });
+    }
+  },
+  // Delete All Guides
+  async delete_guide(request, res) {
+    // Pega todos os paramentros da rota e colocar na variavel
+    const { id_guide } = request.params;
+    // Pega parametros do Headers para variavel
+    const cod_user = request.headers.authorization;
+    // Consulta quantos tem no banco
+    const user = await connection("RPG_users")
+      .select("id")
+      .where("cod", cod_user)
+      .limit(1);
+    // Veriricar se encotrou no banco o usuário
+    if (user.length > 0) {
+      // Consulta se a guide existe
+      const guide = await connection(table_db)
+        .select("id_user")
+        .where("id", id_guide)
+        .limit(1);
+      // Verifica se encontrou uma guide
+      if (guide.length > 0) {
+        // Verifica se o usuário que solicitou pode apagar essa guide
+        if (user[0].id === guide[0].id_user) {
+          await connection(table_db).where("id", id_guide).delete();
+          // Resposta
+          return res.json({
+            msg: "SUCCESS",
+          });
+        } else {
+          // Resposta
+          return res.status(400).json({
+            msg: "Usuário não autorizado",
+          });
+        }
+      } else {
+        // Resposta
+        return res.status(404).json({
+          msg: "Personagem não encontrado",
         });
       }
     } else {
