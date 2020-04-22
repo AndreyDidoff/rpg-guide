56eb3d9bef21b04d4e365f45dae36bcedf3021b4
Andrey Didoff <andrey.didoff@avdata.com.br>
2020-04-22 02:18:11 -0300

    Add Magics

---
+++ backend/src/Routes/routes_guide_magics.js
@@ -0,0 +1,76 @@
+// Requires
+const express = require("express");
+const { celebrate, Segments, Joi } = require("celebrate");
+// Controllers
+const guide_magics = require("../controllers/guide_magics");
+// Variables
+const routes_guide_magics = express.Router();
+/*
+ * guide_magics
+ */
+// Create guide_magics
+routes_guide_magics.post(
+  "/magics",
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required(),
+    }).unknown(),
+    [Segments.BODY]: Joi.object().keys({
+      id_guide: Joi.number().required(),
+      id_magic: Joi.number().required(),
+      vocal_text: Joi.string(),
+      material_text: Joi.string(),
+      gestual_text: Joi.string(),
+      item_text: Joi.string(),
+      description_user: Joi.string(),
+    }),
+  }),
+  guide_magics.create
+);
+// Select All guide_magicss
+routes_guide_magics.get(
+  "/magics",
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required(),
+    }).unknown(),
+    [Segments.BODY]: Joi.object().keys({
+      id_guide: Joi.number().required(),
+    }),
+  }),
+  guide_magics.select_all_magics
+);
+// Update guide_magics
+routes_guide_magics.put(
+  "/magics",
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required(),
+    }).unknown(),
+    [Segments.BODY]: Joi.object().keys({
+      id_guide: Joi.number().required(),
+      id_magic: Joi.number().required(),
+      vocal_text: Joi.string(),
+      material_text: Joi.string(),
+      gestual_text: Joi.string(),
+      item_text: Joi.string(),
+      description_user: Joi.string(),
+    }),
+  }),
+  guide_magics.update_magic
+);
+// Delete guide_magics
+routes_guide_magics.delete(
+  "/magics/:id_magic",
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required(),
+    }).unknown(),
+    [Segments.PARAMS]: Joi.object().keys({
+      id_magic: Joi.number().required(),
+    }),
+  }),
+  guide_magics.delete_magic
+);
+// Export routes_guide_magics
+module.exports = routes_guide_magics;
--- backend/src/app.js
+++ backend/src/app.js
@@ -8,6 +8,7 @@ const routes_guide_armor = require("./Routes/routes_guide_armor");
 const routes_guide_helmos = require("./Routes/routes_guide_helmos");
 const routes_guide_itens = require("./Routes/routes_guide_itens");
 const routes_guide_language = require("./Routes/routes_guide_languages");
+const routes_guide_magics = require("./Routes/routes_guide_magics");
 const routes_guide_others = require("./Routes/routes_guide_others");
 const routes_guide_shields = require("./Routes/routes_guide_shields");
 const routes_guide_weapons = require("./Routes/routes_guide_weapons");
@@ -25,6 +26,7 @@ app.use(routes_guide_armor); // Routes Armor
 app.use(routes_guide_helmos); // Routes Helmos
 app.use(routes_guide_itens); // Routes Itens
 app.use(routes_guide_language); // Routes Languages
+app.use(routes_guide_magics); // Routes Magics
 app.use(routes_guide_others); // Routes Others
 app.use(routes_guide_shields); // Routes Shields
 app.use(routes_guide_weapons); // Routes Weapons
